faa7d78a0f180def3fa6d92b9390fd6ba0bdd1f4
Nitin Verma <nitin.matrix@gmail.com>
2013-02-01 23:26:35 +0000

    added export TWITTER_NLP

--- extractEntities.sh
+++ extractEntities.sh
@@ -4,6 +4,8 @@ curr_pwd=${PWD}
 cd $(dirname $0)
 script_pwd=${PWD}
 
+export TWITTER_NLP=${script_pwd}
+
 cat /dev/stdin | perl -pe 's/[^[:ascii:]]//g' | python python/ner/extractEntities2_json.py $@
 
 cd $curr_pwd
